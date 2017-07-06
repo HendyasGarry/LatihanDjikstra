c191ad36d6999b96f82f5114403856a98f1fd01e
User\GARRY <garryhendyas@gmail.com>
2017-07-06 21:30:40 +0700

    bikin script untuk tiap node

--- New Unity Project/Assets/Scripts/Nodes.cs
+++ New Unity Project/Assets/Scripts/Nodes.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Nodes : MonoBehaviour {
 
+    public int node;
+    public float[] distance=new float [4];
+
 	// Use this for initialization
 	void Start () {
 		
