981e1f2eb85e10ba162a0e87ff6f0e97499c496d
User\GARRY <garryhendyas@gmail.com>
2017-07-07 22:16:58 +0700

    buntu cara implementasi dijkstra nya, upload apa adanya dulu

---
+++ New Unity Project/Assets/Scripts/Dijkstra.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dijkstra : MonoBehaviour {
+
+    public GameObject[] GO= new GameObject [16];
+    int[] visited = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int[] cost=new int[16];
+
+	// Use this for initialization
+	/*void Start () {
+        for (int i = 0; i < 16; i++)
+        {
+            
+        }
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
